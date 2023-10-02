a5b6f86dc061c8768decc33e2f806f83eaf40b0a
Matias <matiasandinoma@gmail.com>
2023-10-01 22:36:23 -0300

    Se listan elementos, y se agrego AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
 
 function GifExpertApp() {
+  const [categories, setCategories] = useState(['Los Simpson', 'Path of Exile', 'Mortal Kombat']);
 
   return (
     <>
-    <h2>GifExpertApp</h2>
-    <hr />
+      <h1>GifExpertApp</h1>
+      <AddCategory setCategories={ setCategories }/>
+      <hr />
+      <ul>
+        {
+          categories.map(category => <li key={category}>{category}</li>)
+        }
+      </ul>
     </>
   )
 }
